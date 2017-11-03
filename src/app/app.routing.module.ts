3efac9d82f1b2d34b186187e0553add228174b76
J.R. Ruggiero <fleury14@comcast.net>
2017-11-03 10:25:58 -0700

    completed initial routing module

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+import { TipComponent } from './components/tip/tip.component';
+
+const routes = [{
+    path: 'home',
+    component: LandingComponent
+},
+{
+    path: 'login',
+    component: LoginComponent
+},
+{
+    path: 'tip',
+    component: TipComponent
+},
+{
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+}];
+
+@NgModule({
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
+})
+
+export class AppRoutingModule {
+
+}
+
+export const RoutingComponents = [ LandingComponent, LoginComponent, TipComponent ];
