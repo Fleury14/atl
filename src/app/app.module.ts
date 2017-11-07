c3af7a6228c8df1625e388c149008789a337c50c
J.R. Ruggiero <fleury14@comcast.net>
2017-11-07 14:11:34 -0800

    had to reinstall firebase and angularfire2 but still got a weird error. will proceed normally for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule, RoutingComponents } from './app.routing.module';
 
+// import angular fire modules
+import { environment } from './../environments/environment';
+import { AngularFireModule} from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 
@@ -13,7 +18,9 @@ import { NavComponent } from './components/nav/nav.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
