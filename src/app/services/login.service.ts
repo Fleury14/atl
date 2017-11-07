97e14cfcdd9b0e1e508c25f19ba9198d5f5b2ba6
J.R. Ruggiero <fleury14@comcast.net>
2017-11-07 14:30:27 -0800

    created login service

---
+++ src/app/services/login.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+Injectable()
+
+export class LoginService {
+
+    // declare logged in user as observable
+    private _loggedInUser: Observable<firebase.User>;
+
+    // greab the logged in user as an observable from the fire auth service
+    constructor( private _authService: AngularFireAuth ) {
+        this._loggedInUser = _authService.authState;
+    }
+
+    public login(): Promise<any> {
+        return this._authService.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider);
+    }
+
+    public logout(): Promise<any> {
+        return this._authService.auth.signOut();
+    }
+}
