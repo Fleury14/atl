40bb1ee4b75d09e22f6e92eb1dade00675bc1f7d
J.R. Ruggiero <fleury14@comcast.net>
2017-11-08 11:45:16 -0800

    commented login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,3 +1,5 @@
+// This is the login service. Any component that needs the user's login information will need to import this and
+// inject it in that components constructor
 import { Injectable } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
@@ -12,21 +14,27 @@ export class LoginService {
     private _loggedInUser: Observable<firebase.User>;
 
 
-    // greab the logged in user as an observable from the fire auth service
+    // grab the logged in user as an observable from the fire auth service
+    // Note that the way an instance of angularfireauth is declared in the constructor. if you want to inject
+    // a service this is how you would do it
     constructor( private _authService: AngularFireAuth ) {
         this._loggedInUser = _authService.authState;
 
     }
 
+    // this is the method for logging in. it returns a promise and uses the angularfire signinwithpopup
     public login(): Promise<any> {
         console.log('Logging in via google popup...');
         return this._authService.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider);
     }
 
+    // this is the method for logging out.
     public logout(): Promise<any> {
         return this._authService.auth.signOut();
     }
 
+    // this is the method for getting the current logged in user. note that this returns a rather large observable object, so any time you
+    // want to grab it, you may want to .map the results into a more manageable object
     public getLoggedInUser(): Observable<firebase.User> {
         return this._loggedInUser;
     }
