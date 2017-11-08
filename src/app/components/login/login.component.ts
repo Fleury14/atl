d9fc0410c35ea86f448a15c915119996ae12d4c1
J.R. Ruggiero <fleury14@comcast.net>
2017-11-08 11:50:03 -0800

    commented login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,6 +15,9 @@ export class LoginComponent implements OnInit {
     public loggedInUser;
     public currentDate: Date;
 
+    // by using oninit, we call the loggedinuser method as soon as the page loads. we dont map the user object right now
+    // but in the future, this would be a good idea. we then subscribe to it so that the loggedinuser updates after
+    // user changes.
     ngOnInit() {
         this._loginService.getLoggedInUser()
             .subscribe( user => {
@@ -26,6 +29,7 @@ export class LoginComponent implements OnInit {
 
     }
 
+    // calls the login and logout methods of the login service
     public login() {
         this._loginService.login();
     }
@@ -34,5 +38,6 @@ export class LoginComponent implements OnInit {
         this._loginService.logout();
     }
 
+    // inject the login service into this component
     constructor( private _loginService: LoginService ) {}
 }
