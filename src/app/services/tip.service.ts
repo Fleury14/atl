9915c9e0d0a327e8c9f0de6c7ff8bb909c8de9cf
J.R. Ruggiero <fleury14@comcast.net>
2017-11-13 20:14:11 -0800

    commented tip service

--- src/app/services/tip.service.ts
+++ src/app/services/tip.service.ts
@@ -13,18 +13,24 @@ export class TipService {
         console.log('Tip service initialized...');
     }
 
+    // this is the method to call the back end api and get all the tips from the database, and .map it to a json
     getTasks() {
         return this.http.get('http://localhost:3000/api/tips')
             .map(res => res.json());
     }
 
-    public addTip(newTip) {
+    // this calls the api to add a new tip to the database that is sent in
+    public addTip(newTip: ITip) {
+        // create a new set of headers and add the content type
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
+
+        // return the post request mapped to a json
         return this.http.post('http://localhost:3000/api/tip', JSON.stringify(newTip), {headers: headers})
             .map(res => res.json);
     }
 
+    // this method deletes the tip given the correct id
     deleteTip(id) {
         return this.http.delete('http://localhost:3000/api/tip/' + id)
             .map(res => res.json());
