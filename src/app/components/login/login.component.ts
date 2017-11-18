61b51478cc0724f0b30e6312006268478ccf3f05
J.R. Ruggiero <fleury14@comcast.net>
2017-11-18 12:31:42 -0800

    comminet login component, made one method private

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -15,15 +15,15 @@ import IUser from './../../interfaces/user.interface';
 
 export class LoginComponent implements OnInit {
 
-    public loggedInUser;
+    public loggedInUser; // will contain the user info
     public currentDate: Date;
-    public tips = [];
-    public userIdResult;
-    public viewPassResult;
-    public canViewTips = false;
-    public idChecker = 'testuser';
-    public uidDocument;
-    public userTips = [];
+    public tips = []; // DEPRECATED: will contain all docs in the database
+    public userIdResult; // flag to denote if the user is in the database
+    public viewPassResult; // flag to denote if the user has a viewing password.
+    public canViewTips = false; // flag to make sure user can view tips. is ALWAYS false on reload
+    public idChecker = 'testuser'; // DEBUG: testing purposes
+    public uidDocument; // will contain user's appropriate document once credentials are verified
+    public userTips = []; // will contain user's stored tips from DB once verified
 
     // ngModels for password entry
     public pass1: string;
@@ -32,7 +32,7 @@ export class LoginComponent implements OnInit {
 
     // by using oninit, we call the loggedinuser method as soon as the page loads. we dont map the user object right now
     // but in the future, this would be a good idea. we then subscribe to it so that the loggedinuser updates after
-    // user changes.
+    // user changes. We also immedialy run a check to make sure the user is in the database
     ngOnInit() {
         this._loginService.getLoggedInUser()
             .subscribe( user => {
@@ -56,6 +56,7 @@ export class LoginComponent implements OnInit {
         this._loginService.login();
     }
 
+    // logout with a confirm to verify
     public logout() {
         if (confirm('Do you really wanna leave?')) {
             this._loginService.logout();
@@ -63,6 +64,8 @@ export class LoginComponent implements OnInit {
 
     }
 
+    // method to make sure that the logged in user has both an id, and a viewpass associated with that id
+    // returning false on any of the two nested methods will not allow the user to view their tips
     public dataCheck(uid) {
 
         if (!uid) {
@@ -80,6 +83,9 @@ export class LoginComponent implements OnInit {
 
     }
 
+    // this is the function of the button that only appears once its been verified that the logged in user is in
+    // the database and has a password. it asks for the viewing password in an prompt and then checks the database to make sure they match
+    // if it does, it instantiates the array of tips with all tips that are inside that document
     public viewTips() {
         const password = prompt('Input your viewing password');
         this._tipService.verifyViewPass(this.loggedInUser.uid, password)
@@ -87,13 +93,15 @@ export class LoginComponent implements OnInit {
             if (res === false) {
                 alert('Incorrect Password');
             }
-            this.getTipsByUid();
+            this._getTipsByUid();
             this.canViewTips = res;
 
         });
     }
 
-    public getTipsByUid() {
+    // method to pull all tips associates with a user id.
+    // THIS SHOULD ONLY BE CALLED WHEN PASSWORD HAS BEEN VERIFIED
+    private _getTipsByUid() {
         this._tipService.getTipsByUid(this.loggedInUser.uid)
         .subscribe( res => {
             this.userTips = res.tips;
@@ -101,6 +109,7 @@ export class LoginComponent implements OnInit {
         });
     }
 
+    // method to call the tipservce and make sure that the logged in user has a document in the database with their userid
     public userIdCheck(id) {
         this._tipService.checkUser(id)
         .subscribe(result => {
@@ -110,6 +119,7 @@ export class LoginComponent implements OnInit {
 
     }
 
+    // calls the tip service to make sure the document associated with the users id also has a viewpass
     public viewPassCheck(id) {
         this._tipService.checkViewPass(id)
         .subscribe(result => {
@@ -117,12 +127,16 @@ export class LoginComponent implements OnInit {
         });
     }
 
+    // method to add a new user to the database
     public submitNewUser() {
+
+        // make sure the password is both not blank and matches
         if (this.pass1 === '' || (!this.pass1)) {
             this._passwordError = true;
             return;
         }
 
+        // create the new IUser that will be posted on the DB
         const newUser: IUser = {
             name: this.loggedInUser.displayName,
             uid: this.loggedInUser.uid,
@@ -130,6 +144,7 @@ export class LoginComponent implements OnInit {
             tips: []
         };
 
+        // call the tip service to add the user
         this._tipService.addUser(newUser)
         .subscribe(data => {
             this.pass1 = '';
